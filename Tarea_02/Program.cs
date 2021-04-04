50d9bd753f0e7a7869f93166531a712b878639c7
Edison Villablanca <edissonbg01@gmail.com>
2021-04-03 21:34:15 -0300

    Agregar archivos de proyecto.

---
+++ Tarea_02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escriba un numero para saber si es Par o impar");
+
+            int numero = Convert.ToInt32(Console.ReadLine());
+            
+            if ((numero % 2) == 0)
+            {
+
+                Console.WriteLine("==============================");
+                Console.WriteLine("     Tu numero es par");
+                Console.WriteLine("==============================");
+            }
+            else
+            {
+                Console.WriteLine("==============================");
+                Console.WriteLine("     Tu numero es impar");
+                Console.WriteLine("==============================");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
